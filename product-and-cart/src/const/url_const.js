ba81d9e43fc757f8f08b4c2b986287d4bb22ad87
Yoo-gyeongjin <96333715+Yoo-gyeongjin@users.noreply.github.com>
2022-11-10 23:47:52 +0900

    Delete product-and-cart directory

--- product-and-cart/src/const/url_const.js
+++
@@ -1,6 +0,0 @@
-const BASE_URL = '/api'
-
-export default {
-  LOGIN: BASE_URL + '/login',
-  GET_TOKEN_REFRESH: BASE_URL + '/refreshtoken'
-}
--- product-and-cart/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'vuex'
-import { login } from './module/login'
-// import Vuex from vuex => vuex 3.0
-
-export const store = createStore({
-  modules: {
-    login
-  }
-})
